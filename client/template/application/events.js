1e14fea7c981410175f666f93c6e154780f65671
Suk Lee <Mrl33@Suks-MacBook-Pro.local>
2015-08-11 16:50:20 -0500

    Facebook login button and log out button

--- client/template/application/events.js
+++ client/template/application/events.js
@@ -1,4 +1,23 @@
-  Template.Layout.events({
+Template.logInScreen.events({
+    'click #facebookButton': function(event) {
+        Meteor.loginWithFacebook({}, function(err){
+            if (err) {
+                throw new Meteor.Error("Facebook login failed");
+            }
+        });
+    },
+
+    'click #logout': function(event) {
+        Meteor.logout(function(err){
+            if (err) {
+                throw new Meteor.Error("Logout failed");
+            }
+        })
+    }
+});
+
+
+  Template.layout.events({
     'click a.logout': function() {
       Meteor.logout(function() {
         // Redirect to login
@@ -8,10 +27,5 @@
     }
   });
 
-Template.dashboard.events({
-  'click [data-logout]': function (e, tmpl) {
-    Meteor.logout();
-  }
 
-});
 
---
+++ server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '1012865308735710',
+    secret: '4aef2e24e00b4c3dbb503c4f8488e93e'
+});
